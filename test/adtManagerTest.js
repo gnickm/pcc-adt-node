040fb4bd53ca3ed8b8c6d78ed2463c4a5f52f5eb
Nick Mitchell <eb.nickm@gmail.com>
2016-09-19 21:31:34 +0000

    Initial cut of handle() function with test

--- test/adtManagerTest.js
+++ test/adtManagerTest.js
@@ -5,6 +5,7 @@
 var expect = require('chai').expect;
 
 var adtman = require('../lib/adtManager');
+var hl7Util = require('../lib/hl7Util.js');
 
 //--------------------------------------------------------------------------
 
@@ -84,4 +85,44 @@ describe('ADT Manager Functions', function() {
             done();
         });
     });
+
+    describe('handle()', function() {
+        it('should fail if missing EVN segment (required for every message type)', function(done) {
+            var hl7 =
+                "MSH|^~\\&|SNDAPPL|snd_fac|RECAPPL|rec_fac|20070208165451.447- 0500||ADT^A03|110A35A09B785|P|2.5\r" +
+                // "EVN|A03|200702080406|||PointClickCare|200702080406\r" +
+                "PID|1||99269^^^^FI~123321^^^^PI||Berk^Ailsa||19400503|F|||579 5 PointClickCare Street^^Lakeview^OH^90210||^PRN^PH^^^^^^^^^(937) 8432794|||||04254|275-32-9550\r" +
+                "PV1|1|N|100^104^A^ABC2PREV0021^^N^100^1||||G45670 ^Haenel^Mary- Ann|||||||||||0||||||||||||||||||||||||||20070207 0403-0500|200702080406-0500\r" +
+                "ZEV|2001|200702080406|PointClickCare";
+
+            hl7Util.parseString(hl7, function(err, parsedMessage) {
+                expect(err).to.be.null;
+                expect(parsedMessage).to.not.equal(null);
+                adtman.handle(parsedMessage, function(err) {
+                    expect(err).to.not.equal(null);
+                    done();
+                });
+            });
+
+        });
+        it('should fail if missing PID segment (required for every message type)', function(done) {
+            var hl7 =
+                "MSH|^~\\&|SNDAPPL|snd_fac|RECAPPL|rec_fac|20070208165451.447- 0500||ADT^A03|110A35A09B785|P|2.5\r" +
+                "EVN|A03|200702080406|||PointClickCare|200702080406\r" +
+                // "PID|1||99269^^^^FI~123321^^^^PI||Berk^Ailsa||19400503|F|||579 5 PointClickCare Street^^Lakeview^OH^90210||^PRN^PH^^^^^^^^^(937) 8432794|||||04254|275-32-9550\r" +
+                "PV1|1|N|100^104^A^ABC2PREV0021^^N^100^1||||G45670 ^Haenel^Mary- Ann|||||||||||0||||||||||||||||||||||||||20070207 0403-0500|200702080406-0500\r" +
+                "ZEV|2001|200702080406|PointClickCare";
+
+            hl7Util.parseString(hl7, function(err, parsedMessage) {
+                expect(err).to.be.null;
+                expect(parsedMessage).to.not.equal(null);
+                adtman.handle(parsedMessage, function(err) {
+                    expect(err).to.not.equal(null);
+                    done();
+                });
+            });
+
+        });
+    });
+
 });
